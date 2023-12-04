00ff873360361cae281ecb56a336bf764a511c01
PaoloTarcisio <paoloc50@gmail.com>
2023-12-04 17:42:37 +0100

    completamento html e css

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let nome = document.getElementById('NomeCognome')
+
+const chilometri = document.getElementById('Km');
+const kmInNumber = parseInt (chilometri);
+
+const età = document.getElementById('FasciaEta');
+const etàInNumber = parseInt (età);
+
+
+document.getElementById('MyButton').addEventListener('click', function() {
+    document.getElementById('nominativo').append = nome.value;
+});
+
