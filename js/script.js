6530ea9a63ea4f2fa2f484045cd36ce9e49505b2
PaoloTarcisio <paoloc50@gmail.com>
2023-12-05 10:12:21 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
-let nome = document.getElementById('NomeCognome')
+const MyButton = document.getElementById('MyButton');
 
-const chilometri = document.getElementById('Km');
-const kmInNumber = parseInt (chilometri);
+MyButton.addEventListener('click', function() {
+    const nome = document.querySelector('#NomeCognome');
+    console.log('nome', nome.value, typeof nome);
+    const prova = document.getElementById('NomeCognomeInfo').innerHTML = `${nome.value}`
 
-const età = document.getElementById('FasciaEta');
-const etàInNumber = parseInt (età);
+    const offerta = document.querySelector('#offerta').innerHTML = 'Biglietto Standart'
+
+    const carrozza = document.querySelector('#carrozza').innerHTML = ((Math.floor(Math.random() * 10)) + 1);
+    console.log('carrozza', carrozza.value, typeof carrozza);
+
+    const codiceCP = document.querySelector('#codiceCP').innerHTML = (Math.floor(Math.random() * 100000));
+    console.log('codiceCP', codiceCP.value, typeof codiceCP);
+
+    const chilometri = document.querySelector('#Km');
+    const kmInNumber = parseInt(chilometri.value);
+    const eta = document.querySelector('#FasciaEta');
+    let prezzo = (kmInNumber * 0.21)
+    let prezzoInNumber = parseInt(prezzo);
+
+    if (eta.value == '2') {
+        prezzo *= 0.8;
+        console.log ('prezzo', prezzo, 'minorenne');
+        const costo = document.querySelector('#costo').innerHTML = `${prezzo}`;
+    }
+
+    if (eta.value == '3') {
+        prezzo *= 0.6;
+        console.log ('prezzo', prezzo, 'pensionato');
+        const costo = document.querySelector('#costo').innerHTML = `${prezzo}`;
+
+    }
+
+    else { 
+        const costo = document.querySelector('#costo').innerHTML = `${prezzo}`;
+    };
 
 
-document.getElementById('MyButton').addEventListener('click', function() {
-    document.getElementById('nominativo').append = nome.value;
 });
 
+// let nominativo = document.getElementById('NomeCognome');
+// console.log('nominativo', nominativo, typeof nominativo);
